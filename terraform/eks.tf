a4911e065fc73303e8f422c8c8d567152ec06132
iam-vinod <vinodmethri19@gmail.com>
2025-03-27 17:31:54 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,20 +1,18 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "18.0.0" # Or the latest stable version
+
   cluster_name    = "eks-cluster"
   cluster_version = "1.27"
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.public_subnets
-
-  enable_irsa = true
-
-  node_groups = {
-    eks_nodes = {
+  eks_managed_node_groups = {
+    eks-node-group = {
       desired_capacity = 2
-      min_capacity     = 1
       max_capacity     = 3
-
-      instance_types = ["t3.medium"]
+      min_capacity     = 1
+      instance_types   = ["t3.medium"]
     }
   }
 }
